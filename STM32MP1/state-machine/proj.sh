ccfd1bb157276214852d2439151fd2a8bbedf4ac
ian <iyurychu@ualberta.ca>
2024-04-06 23:00:17 -0600

    added dockerfile for building linux kernel and sdk

---
+++ STM32MP1/state-machine/proj.sh
@@ -0,0 +1,6 @@
+sudo docker build \
+    --build-arg SDK_PATH=en.SDK-x86_64-stm32mp1-openstlinux-6.1-yocto-mickledore-mp1-v23.06.21.tar.gz \
+    --build-arg SRC_PATH=en.sources-stm32mp1-openstlinux-6.1-yocto-mickledore-mp1-v23.06.21.tar.gz \
+    -t iany/state-machine .
+
+sudo docker run --rm -it --privileged iany/state-machine:latest bash
