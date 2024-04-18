12bfeabd899d730011c453f794c3da556e0659b0
jeel shah <jeelshah911@gmail.com>
2024-04-18 14:35:38 -0600

    issue4

--- statemachine/main.c
+++ statemachine/main.c
@@ -3,21 +3,22 @@
 #include <pthread.h>
 
 #include "statemachine.h"
-
+#include "udp.h" 
 void* udpcomms_thread(void* arg);
 
 
 int main() {
     
-    pthread_t statemachine_tid, udpcomms_tid;
+    pthread_t statemachine_tid, udp_tlm_tid, udp_cmd_tid;
 
     // Create threads for statemachine and udp.
     pthread_create(&statemachine_tid, NULL, statemachine_thread, NULL);
-    // pthread_create(&udpcomms_tid, NULL, udpcomms_thread, NULL);
+    pthread_create(&udp_tlm_tid, NULL, udp_tlm_thread_fn, NULL);
+    pthread_create(&udp_cmd_tid, NULL, udp_cmd_thread_fn, NULL);
 
     
     pthread_join(statemachine_tid, NULL);
-    // pthread_join(udpcomms_tid, NULL);
-
+    pthread_join(udp_tlm_tid, NULL); 
+    pthread_join(&udp_cmd_tid, NULL);
     return 0;
 }
--- statemachine/udp.c
+++ statemachine/udp.c
@@ -1,5 +1,3 @@
-//udp client server program that prints simple messages
-//A program that can send commands and recieve telemetry.
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -8,9 +6,24 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
+#include <errno.h>
+
 #define SERVER_PORT 3000
 #define CLIENT_PORT 4000
 #define CMD_PACKET_LEN 512
+
+void init_udp_sockets(int *server_fd, struct sockaddr_in *server_addr) {
+    // Create socket for server
+    *server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // Bind the socket
+    memset(server_addr, 0, sizeof(*server_addr));
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr->sin_port = htons(CLIENT_PORT);
+    bind(*server_fd, (struct sockaddr *)server_addr, sizeof(*server_addr));
+}
 
 void* udp_tlm_thread_fn(void* arg) {
     int client_fd;
@@ -19,16 +32,16 @@ void* udp_tlm_thread_fn(void* arg) {
 
     client_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-   
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(SERVER_PORT);
 
- 
+    // Send telemetry data
     sendto(client_fd, telemetry_data, strlen(telemetry_data), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     close(client_fd);
+    sleep_ms(1000);
 }
 
 void* udp_cmd_thread_fn(void* arg) {
@@ -36,15 +49,8 @@ void* udp_cmd_thread_fn(void* arg) {
     struct sockaddr_in server_addr, client_addr;
     char command_data[CMD_PACKET_LEN];
 
-    // Create socket for server
-    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    // Bind the socket
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(CLIENT_PORT);
-    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    // Initialize UDP sockets
+    init_udp_sockets(&server_fd, &server_addr);
 
     printf("Server listening on port %d\n", CLIENT_PORT);
 
@@ -52,15 +58,19 @@ void* udp_cmd_thread_fn(void* arg) {
     socklen_t client_len = sizeof(client_addr);
     recvfrom(server_fd, command_data, CMD_PACKET_LEN, 0, (struct sockaddr *)&client_addr, &client_len);
     printf("Received command from client: %s\n", command_data);
-   
- 
+
     close(server_fd);
 }
 
 void* udpcomms_thread(void* arg) {
-    
+    // Additional code for UDP communications thread
     return NULL;
 }
 
-
+void sleep_ms(int milliseconds) {
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    while (nanosleep(&ts, &ts) && errno == EINTR);
+}
 
